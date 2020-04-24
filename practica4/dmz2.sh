6ced2fac4cf15b853f04a680b8eac544e7087357
Sergi Castella <sergicaste97@gmail.com>
2020-04-24 14:21:12 +0200

    push

--- practica4/dmz2.sh
+++ practica4/dmz2.sh
@@ -29,4 +29,17 @@ iptables -t nat -A POSTROUTING -s 172.21.0.0/16 -o ens33 -j MASQUERADE
 iptables -t nat -A POSTROUTING -s 172.22.0.0/16 -o ens33 -j MASQUERADE
 
 # Regles DMZ
+# Permetre l'acces al ldap desde el router ( te la ip 5 ja que m'he oblidat d'obrir un els dockers de DMZ )
+iptables -t nat -A PREROUTING -p tcp --dport 389 -i ens33 -j DNAT --to 172.22.0.5:389
+iptables -t nat -A PREROUTING -p tcp --dport 636 -i ens33 -j DNAT --to 172.22.0.5:636
+
+# Permetre obtenir tickets kerberos desde un port especific en el router
+iptables -t nat -A PREROUTING -p tcp --dport 88 -i ens33 -j DNAT --to 172.22.0.3:88
+iptables -t nat -A PREROUTING -p tcp --dport 543 -i ens33 -j DNAT --to 172.22.0.3:543
+iptables -t nat -A PREROUTING -p tcp --dport 749 -i ens33 -j DNAT --to 172.22.0.3:749
+iptables -t nat -A PREROUTING -p tcp --dport 544 -i ens33 -j DNAT --to 172.22.0.3:544
+
+# Permetre montar un samba desde el router
+iptables -t nat -A PREROUTING -p tcp --dport 139 -i ens33 -j DNAT --to 172.22.0.4:139
+iptables -t nat -A PREROUTING -p tcp --dport 445 -i ens33 -j DNAT --to 172.22.0.4:445
 
